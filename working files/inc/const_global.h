4c869e909d38f4e94c98d1bc2824aad95f787a48
Taras Malynovsky <malynovsky@ukr.net>
2018-01-03 17:34:01 +0200

    03.01.18: Підправив версію у cons_global.h з v.9.1.1.0 на v.9.1.1.1

--- working files/inc/const_global.h
+++ working files/inc/const_global.h
@@ -1612,7 +1612,7 @@ NUMBER_ODYNYCI_VYMIRJUVANNJA
 #define VERSIA_PZ                                             9
 #define MODYFIKACIA_VERSII_PZ                                 1
 #define ZBIRKA_VERSII_PZ                                      1
-#define ZBIRKA_PIDVERSII_PZ                                   0
+#define ZBIRKA_PIDVERSII_PZ                                   1
 /*****************************************/
 
 /*****************************************/
